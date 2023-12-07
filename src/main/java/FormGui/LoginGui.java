56ccee44ff7351cc29a0852c0245116fee6a8b6b
Tales Palma <tales.palma_2022@outlook.com>
2023-12-07 13:23:40 -0300

    2.3 Tela de login funcionando

--- src/main/java/FormGui/LoginGui.java
+++ src/main/java/FormGui/LoginGui.java
@@ -1,9 +1,14 @@
 package FormGui;
 
+import org.example.Aluno.DTOAluno;
+import org.example.LoginDAO;
 import org.example.Tools;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
 
 public class LoginGui extends JFrame{
     private JPanel Main;
@@ -20,13 +25,18 @@ public class LoginGui extends JFrame{
         entrarButton.addActionListener(v->{
             String email = emailtextField.getText().toString().trim();
             String senha = senhatextField2.getText().toString().trim();
+            LoginDAO loginDAO = new LoginDAO();
+            Map<String,String> mapLogin= loginDAO.selectDate();
 
-            if(email.equals("tales@gmail.com") && senha.equals("123")){
-                this.dispose();
-                GuiForm guiForm = new GuiForm();
-            }
-            else{
-                JOptionPane.showMessageDialog(null,"SENHA OU EMAIL INCORRETOS");
+            try {
+                if (mapLogin.get(email).toString().equals(senha)) {
+                    this.dispose();
+                    GuiForm guiForm = new GuiForm();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Senha ou email invalido");
+                }
+            }catch (Exception ex){
+                JOptionPane.showMessageDialog(null,"Digite algo nos campos por favor");
             }
 
         });
--- src/main/java/org/example/LoginDAO.java
+++ src/main/java/org/example/LoginDAO.java
@@ -1,4 +1,46 @@
 package org.example;
 
+import ConexaoBd.ConnectBD;
+
+import javax.swing.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginDAO {
+    private String email , senha ;
+    private int id ;
+
+
+    public Map selectDate(){
+        ConnectBD connectBD = new ConnectBD();
+        Statement statement = connectBD.getStatement();
+        ResultSet resultSet = null ;
+        Map<String,String> map = new HashMap<>();
+        String sql = "SELECT * FROM Login";
+        try{
+           resultSet = statement.executeQuery(sql);
+            while(resultSet.next()){
+                Object[] objects = new Object[3];
+                int cont=0;
+                for(int i = 1 ; i<=3 ; i++){
+                   objects[cont]=resultSet.getString(i);
+                   cont++;
+                }
+
+                map.put(objects[1].toString(),objects[2].toString());
+            }
+
+        }catch (SQLException ex){
+            JOptionPane.showMessageDialog(null,
+                    "Algum erro occoreu com o banco de dados"  + ex.getMessage());
+        }
+
+
+        return  map;
+    }
+
+
+
 }
